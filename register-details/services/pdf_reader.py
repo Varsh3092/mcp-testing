42e6b40f8a00a5c88f88010a23458d09b2a9e604
Varsh3092 <varshasomasundaram309@gmail.com>
2026-07-03 08:09:45 +0530

    Create pdf_reader.py

---
+++ register-details/services/pdf_reader.py
@@ -0,0 +1,19 @@
+from pypdf import PdfReader
+
+
+def read_pdf(pdf_path: str) -> str:
+    """
+    Extract complete text from PDF.
+    """
+
+    reader = PdfReader(pdf_path)
+
+    full_text = []
+
+    for page in reader.pages:
+        text = page.extract_text()
+
+        if text:
+            full_text.append(text)
+
+    return "\n".join(full_text)
